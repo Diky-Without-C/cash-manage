5c63d2479607657a435b1867c2a4fbb4b3c7fe83
Diky-Without-C <snayzher@gmail.com>
2024-08-25 19:41:02 +0700

    fix: week in month issue

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const WEEK_PER_MONTH = 4;
 export const MONTHS = ["Aug", "Sep", "oct", "Nov", "Des"];
-export const WEEKS = MONTHS.map((month, i) =>
-  Array(WEEK_PER_MONTH).fill(`${month} ${(i % WEEK_PER_MONTH) + 1}`),
-).flat();
+export const WEEKS = MONTHS.flatMap((month) =>
+  Array.from({ length: WEEK_PER_MONTH }, (_, i) => `${month} ${i + 1}`),
+);
