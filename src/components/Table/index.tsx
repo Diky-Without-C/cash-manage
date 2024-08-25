8c39e91ee1f0c775f9af8a46a988db0681f4cef6
Diky-Without-C <snayzher@gmail.com>
2024-08-25 19:46:05 +0700

    feat: table header now pinned on top

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -5,7 +5,7 @@ import { StudentsProps } from "./table.type";
 export default function Table({ students }: StudentsProps) {
   return (
     <section className="h-full w-full">
-      <table className="table table-zebra w-[150rem]">
+      <table className="table table-zebra table-pin-rows w-[150rem]">
         <Header />
         <Body students={students} />
       </table>
