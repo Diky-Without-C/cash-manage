cee1fabe5ea069201869bf7cacad2ba0cc783aa4
Diky-Without-C <snayzher@gmail.com>
2024-08-25 20:12:08 +0700

    fix: table zebra on dark mode issue

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,11 +1,12 @@
 import Header from "./TableHeader";
 import Body from "./TableBody";
 import { StudentsProps } from "./table.type";
+import "./table.css";
 
 export default function Table({ students }: StudentsProps) {
   return (
     <section className="h-full w-full">
-      <table className="table table-zebra table-pin-rows w-[150rem]">
+      <table className="table table-pin-rows w-[150rem]">
         <Header />
         <Body students={students} />
       </table>
