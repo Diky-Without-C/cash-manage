9e510703946e3eed94d9aff8c14d9ccc4f16fc0b
Diky-Without-C <snayzher@gmail.com>
2024-08-31 19:17:07 +0700

    fix: center 404 content

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <main className="flex h-full w-full flex-col items-center">
+    <main className="flex h-full w-full flex-col items-center justify-center">
       <img src={notfound} className="h-auto w-1/2" />
       <h1 className="text-xl font-bold text-gray-900">404 | Page Not Found</h1>
       <Link to="/" className="btn btn-primary btn-sm mt-4 rounded-lg">
