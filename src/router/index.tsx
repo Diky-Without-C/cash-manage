7ed55daf1337aaa91f813d768593b51c436a9a10
Diky-Without-C <snayzher@gmail.com>
2024-08-31 19:13:31 +0700

    feat: add 404 page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,18 @@
 import Home from "@pages/Home";
 import Revenue from "@pages/Revenue";
+import NotFound from "@pages/NotFound";
 import { createBrowserRouter, RouteObject } from "react-router-dom";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <Home />,
+    errorElement: <NotFound />,
   },
   {
     path: "/revenue/",
     element: <Revenue />,
+    errorElement: <NotFound />,
   },
 ];
 
